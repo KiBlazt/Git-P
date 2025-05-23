b99f505c00d60d7d3df9ed5868723e5172398ee8
CivicAtom162201 <soberanisyanni@gmail.com>
2025-05-22 21:08:56 -0600

    0.04

--- yani.py
+++ yani.py
@@ -1,1 +1,9 @@
 print("Buenas")
+# Ejemplo sencillo de Python: Saludar al usuario
+
+def saludar():
+    nombre = input("Introduce tu nombre: ")
+    print("¡Hola, " + nombre + "! Bienvenido a Python.")
+
+if __name__ == "__main__":
+    saludar()
